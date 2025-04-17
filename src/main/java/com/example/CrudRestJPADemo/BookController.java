5283a6d905d65e476e1aaedf8816b9bfd17c4268
Mayur <107352861+Mayurdpatil67@users.noreply.github.com>
2025-04-17 20:42:06 +0530

    Add Rest controller to acces API endpoints

--- src/main/java/com/example/CrudRestJPADemo/BookController.java
+++ src/main/java/com/example/CrudRestJPADemo/BookController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class BookController {
+public class BookController{
 
     @Autowired
     BookRepository bookRepository;
