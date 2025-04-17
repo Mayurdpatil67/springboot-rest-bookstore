13155b8cd120993a32b1d9cf25ef7617230ad780
Mayur <107352861+Mayurdpatil67@users.noreply.github.com>
2025-04-17 20:39:30 +0530

    Class Book to create a Data Model

--- src/main/java/com/example/CrudRestJPADemo/Book.java
+++ src/main/java/com/example/CrudRestJPADemo/Book.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Book {
+public class Book{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
